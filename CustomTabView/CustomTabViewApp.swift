2ebc154238a1b6ff94496ac355bba233540526ec
kalashnikovaYla <kalashnikova-yla@mail.ru>
2023-05-08 20:10:39 +0500

    Initial Commit

---
+++ CustomTabView/CustomTabViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomTabViewApp.swift
+//  CustomTabView
+//
+//  Created by sss on 08.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct CustomTabViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
